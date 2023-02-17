ed9e27955df2b56e3b01d9810ba7a9365f89382b
GuruMahan <iosdeve2022@gmail.com>
2023-02-17 14:03:32 +0530

    Initial Commit

---
+++ SceneStoragePWrapperPOC/SceneStoragePWrapperPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  SceneStoragePWrapperPOCApp.swift
+//  SceneStoragePWrapperPOC
+//
+//  Created by Guru Mahan on 17/02/23.
+//
+
+import SwiftUI
+
+@main
+struct SceneStoragePWrapperPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
